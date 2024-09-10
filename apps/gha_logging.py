417b07000ece948fd97cd69381dc276af34e2d98
cm-fujikawa <fujikawa.takuji@classmethod.jp>
2024-09-11 01:22:37 +0900

    ACTIONS_RUNNER_DEBUG、ACTIONS_STEP_DEBUGを設定

--- apps/gha_logging.py
+++ apps/gha_logging.py
@@ -2,9 +2,16 @@ import logging
 import os
 
 RUNNER_DEBUG = int(os.environ.get("RUNNER_DEBUG", "0"))
+ACTIONS_RUNNER_DEBUG = os.environ.get("ACTIONS_RUNNER_DEBUG")
+ACTIONS_STEP_DEBUG = os.environ.get("ACTIONS_STEP_DEBUG")
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG if RUNNER_DEBUG == 1 else logging.INFO)
+
+
+logger.info(f"ACTIONS_RUNNER_DEBUG: {ACTIONS_RUNNER_DEBUG}")
+logger.info(f"ACTIONS_STEP_DEBUG: {ACTIONS_STEP_DEBUG}")
 
 
 def sub():
